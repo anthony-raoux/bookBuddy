6f07976b25069bf71199d2f589ddc980a77ae380
Salim.ababulgu <salim.ababulgu@laplateforme.io>
2024-05-22 15:49:06 +0200

    Push de sécurité

--- frontend/my-react-app/components/navbar.jsx
+++ frontend/my-react-app/components/navbar.jsx
@@ -9,29 +9,31 @@ import 'bootstrap-icons/font/bootstrap-icons.css'; // Importe toutes les icônes
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-black">
-            <div className="container-fluid">
-                <div className='d-flex align-items-center justify-content-center'>
+            <div className="container-fluid d-flex align-items-center justify-content-between">
+                <div>
                     <Link className="navbar-brand text-light" to="/">Book Buddy</Link>
                     <i className="bi bi-search text-light"></i>
                 </div>
-                <button data-bs-theme="dark" className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/favorites">Favorites</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">Register</Link>
-                        </li>
-                    </ul>
+                <div>
+                    <button data-bs-theme="dark" className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link text-light" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link text-light" to="/favorites">Favorites</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link text-light" to="/login">Login</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link text-light" to="/register">Register</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </nav>
